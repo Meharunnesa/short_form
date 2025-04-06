91aed0b74944a20fea624c8819121202028f5904
Meharunnesa <meharunnesab28@gmail.com>
2025-04-06 14:57:47 +0600

    updated code

--- index.php
+++ index.php
@@ -55,7 +55,48 @@ function shortcode_form(){
   
   
 add_shortcode('short_form', 'shortcode_form');
-  
 
+
+function admin_panel() {
+	add_menu_page(
+		__( 'Custom Menu Title', 'textdomain' ),
+		'Short Form',
+		'manage_options',
+		'myplugin/myplugin-admin.php',
+        'admin_panel_callback',
+        'dashicons-chart-pie',
+		6
+	);
+}
+
+add_action( 'admin_menu', 'admin_panel' );
+  
+function admin_panel_callback() {
+    global $wpdb;
+    $table_name ='short_form';
+    $results = $wpdb->get_results( "SELECT * FROM $table_name", ARRAY_A);
+    
+    //print_r($results);
+
+    echo '<div><h1>User Data Table</h1>';
+    if ( !empty($results) ) {
+        echo '<table class="widefat fixed">';
+        echo '<thead><tr><th>ID</th><th>Name</th><th>Email</th><th>Message</th></tr></thead>';
+        echo '<tbody>';
+        foreach ( $results as $result ) {
+            echo '<tr>';
+            echo '<td>' . esc_html( $result['id'] ) . '</td>';
+            echo '<td>' . esc_html( $result['name'] ) . '</td>';
+            echo '<td>' . esc_html( $result['email'] ) . '</td>';
+            echo '<td>' . esc_html( $result['message'] ) . '</td>';
+            echo '</tr>';
+        }
+        echo '</tbody></table>';
+    } else {
+        echo 'No data found.';
+    }
+    echo '</div>';
+
+}
 
 ?>
